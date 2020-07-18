adca5fdcc24415306f50cf31b05578584ad9b6e7
emre1702 <emre1702@live.de>
2020-07-18 17:46:45 +0200

    Revert "Get new captcha url in loop"
    This reverts commit f1054376606f77e5ed50b593718630fb018fa209.

--- IhkObserver.WpfResultViewer/ViewModels/HomeViewModel.cs
+++ IhkObserver.WpfResultViewer/ViewModels/HomeViewModel.cs
@@ -99,8 +99,6 @@ namespace IhkObserver.WpfResultViewer.ViewModels
         {
             for (int i = 0; i < trys; i++)
             {
-                _observer.GetLoginInformations();
-
                 Bitmap map = _observer.GetLoginCaptcha();
                 Captcha = BitmapConversion.BitmapToBitmapSource(map);
 
@@ -125,6 +123,8 @@ namespace IhkObserver.WpfResultViewer.ViewModels
         private void ExecuteLoadResults()
         {
             _observer.AddSessionId();
+
+            _observer.GetLoginInformations();
 
             TryLogin(5);
         }
